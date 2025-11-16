c64c5e0902e55f6c24d5c1d968fdfb95ba610c71
Phatrycio Miranda <phatrycio137@gmail.com>
2025-11-16 20:32:06 -0300

    propriedades

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,7 +1,9 @@
 export default function TextInput () {
     return (
+        //{} -> Escape, o código dentro será js
+        //maxLenght -> Usado para especificar o número máximo de caracteres
         <div>
-            <textarea />
+            <textarea placeholder={"Koé Paizão? Fala aí pra nós"} maxLength={120}/>
         </div>
     )
 }
