d7c9b608afa849be1d12f325d01442a9b0711c9d
Phatrycio Miranda <phatrycio137@gmail.com>
2025-11-16 21:45:02 -0300

    estilização com css modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import TextInput from "./components/TextInput"
+import styles from './App.module.css'
 
 function App() {
   return (
-  <TextInput />
+  <div className={styles.appContainer}>
+    <TextInput />
+  </div>
 )
 }
 
--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,9 +1,11 @@
+import styles from './TextInput.module.css';
+
 export default function TextInput () {
     return (
         //{} -> Escape, o código dentro será js
         //maxLenght -> Usado para especificar o número máximo de caracteres
         <div>
-            <textarea placeholder={"Koé Paizão? Fala aí pra nós"} maxLength={120}/>
+            <textarea className={styles.input} placeholder={"Koé Paizão? Fala aí pra nós!"} maxLength={120}/>
         </div>
     )
 }
