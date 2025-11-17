0cdafad538bc6ab8f2a381579c5d7328eca2e5a4
Phatrycio Miranda <phatrycio137@gmail.com>
2025-11-17 17:29:08 -0300

    adiconando a lógica do contador de caracteres

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,11 +1,28 @@
+import { useState } from 'react';
 import styles from './TextInput.module.css';
 
-export default function TextInput () {
+export default function TextInput({ placeholder = 'Koé Paizão? Fala aí pra nós!', maxLength = 200, ...props}) {
+    const [text, setText] = useState('');
+
+    function onTextChange(event) {
+    setText(event.target.value);
+  }
+
     return (
         //{} -> Escape, o código dentro será js
         //maxLenght -> Usado para especificar o número máximo de caracteres
+        //onChange -> Cada alteração 
+        //onTextChange -> Mudança a cada caracter
         <div>
-            <textarea className={styles.input} placeholder={"Koé Paizão? Fala aí pra nós!"} maxLength={120}/>
+            <textarea 
+                className={styles.input} 
+                placeholder={placeholder} 
+                maxLength={maxLength}
+                value={text}
+                onChange={onTextChange}
+                {...props}
+            />
+            <p>{text.length} / {maxLength}</p>
         </div>
-    )
+    );
 }
