03410c2e852219404349226bbe1963adfb07b543
Phatrycio Miranda <phatrycio137@gmail.com>
2025-11-20 13:57:56 -0300

    ajustando o componente Tweet

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,16 +1,16 @@
 import styles from './Tweet.module.css'
 
-export default function Tweet(props) {
+export default function Tweet({tweet}) {
     return (
         <div className={styles.tweetContainer}>
-            <img className={styles.avatar} src={'https://github.com//phatryciomn.png'}/>
+            <img className={styles.avatar} src={tweet.user.picture}/>
             <div className={styles.user}>
-                <span className={styles.userName}>Phatrycio Miranda Nascimento</span>
-                <span className={styles.userUserName}>@phatryciomn</span>
-                <span className={styles.date}>Há 12 horas</span>
+                <span className={styles.userName}>{tweet.user.name}</span>
+                <span className={styles.userUserName}>{tweet.user.username}</span>
+                <span className={styles.date}>{tweet.date}</span>
             </div>
             <div className={styles.tweetText}>
-                {props.children}
+                {tweet.text}
             </div>
         </div>
     )
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,22 @@
 import { useState } from 'react';
 import TextInput from "../components/TextInput";
 import Tweet from '../components/Tweet';
+import styles from './Index.module.css';
 
 export default function Index() {
     const [text, setText] = useState('');
     const maxLength = 120;
     const [tweetList, setTweetList] = useState([]);
+
+    const tweet = {
+        date: new Date().toDateString(),
+        text: text,
+        user: {
+            name: 'Phatrycio Miranda Nascimento',
+            userName: '@phatryciomn',
+            picture: 'https://github.com//phatryciomn.png'
+        }
+    }
 
     function onTextChange(event) {
     const text = event.target.value;
@@ -15,29 +26,37 @@ export default function Index() {
     }
 
     function sendTweet() {
-        setTweetList([...tweetList, text])
+        setTweetList([...tweetList, tweet])
     }
 
     return (
         <div>
-            <h1>TreinaTweet</h1>
-            <div>
-                <img src={'https://github.com//phatryciomn.png'} />
+            <h1 className={styles.pageTitle}>TreinaTweet</h1>
+            <div className={styles.tweetContainer}>
+                <img src={'https://github.com//phatryciomn.png'}
+                className={styles.avatar} />
+
                 <TextInput
                 placeholder={'Fala meu peixe, o q ta pegando?'}
+                rows={3}
                 maxLength={maxLength}
                 value={text}
                 onChange={onTextChange}
                 />
             </div>
-            <div>
+            <div className={styles.buttonContainer}>
                 <div>{text.length} / {maxLength}</div>
-                <button onClick={sendTweet}>Tweetar</button>            
+                <button 
+                onClick={sendTweet}
+                className={styles.postButton}
+                disabled={text.length === 0}
+                >Tweetar</button>            
             </div>
-            <ul>
+            <ul className={styles.tweet}>
                 {tweetList.map(tweet => {
                     return (
-                        <li><Tweet children={tweet}/></li>
+                        <li className={styles.tweetListItem}>
+                            <Tweet tweet={tweet}/></li>
                     )
                 })}
             </ul>
